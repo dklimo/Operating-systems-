621eb48fb28df1a0b4a2b0e6cd9c0d44b411991c
Darya <dasha.klim2006@gmail.com>
2025-09-24 17:01:25 +0300

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,14 +2,25 @@
 #include <iostream>
 #include <vector>
 #include "min_max.h"
+#include "average.h" 
 using namespace std;
+double glaverage = 0;
+int glmin = 0, glmax = 0;
+
 DWORD WINAPI min_max_thread(LPVOID lpParam) {
     vector<int>* arr = static_cast<vector<int>*>(lpParam);
     auto result = find_min_max(*arr);
-    cout << "Thread min_max: min = " << result.first << ", max = " << result.second << endl;
+    glmin = result.first;
+    glmax = result.second; 
+    cout << "Thread min_max: min = " << glmin << ", max = " << glmax << endl;
     return 0;
 }
-
+DWORD WINAPI average_thread(LPVOID lpParam) {
+    vector<int>* arr = static_cast<vector<int>*>(lpParam);
+    glaverage = find_average(*arr);
+    cout << "Thread average: avg = " << glaverage << endl;
+    return 0;
+}
 int main() {
     int n;
     cout << "Enter array size: ";
@@ -21,15 +32,29 @@ int main() {
         cin >> A[i];
     }
 
-    DWORD IDMinMax;
+    DWORD IDMinMax, IDAverage; 
     HANDLE hMinMax = CreateThread(NULL, 0, min_max_thread, &A, 0, &IDMinMax);
-    if (hMinMax == NULL) {
-        cerr << "Error creating thread: " << GetLastError() << endl;
+    HANDLE hAverage = CreateThread(NULL, 0, average_thread, &A, 0, &IDAverage);
+    if (!hMinMax || !hAverage) { 
+        cerr << "Error creating threads: " << GetLastError() << endl; 
         return 1;
     }
-
     WaitForSingleObject(hMinMax, INFINITE);
-    CloseHandle(hMinMax);
+    WaitForSingleObject(hAverage, INFINITE); 
 
+    for (int& val : A) {
+        if (val == glmin || val == glmax) {
+            val = static_cast<int>(glaverage);
+        }
+    }
+
+    cout << "Modified array: ";
+    for (int val : A) {
+        cout << val << " ";
+    }
+    cout << endl;
+    CloseHandle(hMinMax);
+    CloseHandle(hAverage); 
     return 0;
 }
+
