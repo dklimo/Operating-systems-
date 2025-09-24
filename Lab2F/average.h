bf04fada56c77697cc823551509dec87a8174e2c
Darya <dasha.klim2006@gmail.com>
2025-09-24 16:54:42 +0300

    Delete Lab2F directory

--- Lab2F/average.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include <utility>
-
-double find_average(const std::vector<int>& arr);
--- Lab2F/min_max.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include <utility>
-
-std::pair<int, int> find_min_max(const std::vector<int>& arr);
