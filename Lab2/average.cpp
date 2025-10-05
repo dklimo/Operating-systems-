7acab636ba520b8987623fe450343f2b12123387
Darya Klimovich <dasha.klim2006@gmail.com>
2025-10-05 10:32:00 +0300

    named constants

--- Lab2/average.cpp
+++ Lab2/average.cpp
@@ -2,13 +2,13 @@
 #include <windows.h>
 
 using namespace std;
-
+const int AVERAGE_CALCULATE_DELAY = 12;
 double find_average(const vector<int>& arr) {
 	if (arr.empty()) return 0.0;
 	long long sum = 0;
 	for (int value : arr) {
 		sum += value; 
-		Sleep(12);
+		Sleep(AVERAGE_CALCULATE_DELAY);
 	}
 	return static_cast<double>(sum) / arr.size();
 }
