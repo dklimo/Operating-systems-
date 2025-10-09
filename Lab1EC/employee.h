fe82e52eb833941086dff92e527ecd43340e15ac
Darya Klimovich <dasha.klim2006@gmail.com>
2025-10-09 14:02:51 +0300

    edit struct

--- Lab1EC/employee.h
+++ Lab1EC/employee.h
@@ -1,16 +1,26 @@
-#include <windows.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
+
+using namespace std;
 
 struct employee {
     int num;
     char name[10];
     double hours;
+
+    bool readFromFile(istream& fin) {
+        return fin.read(reinterpret_cast<char*>(&num), sizeof(num)) && fin.read(name, sizeof(name)) 
+            && fin.read(reinterpret_cast<char*>(&hours), sizeof(hours));
+    }
+    bool writeToFile(ostream& fout) {
+        return fout.write(reinterpret_cast<char*>(&num), sizeof(num)) && fout.write(name, sizeof(name))
+            && fout.write(reinterpret_cast<char*>(&hours), sizeof(hours));
+    }
 };
-void writeBinaryFile(const std::string& filename);
-void writeReport(const std::string& filename);
+void writeBinaryFile(const string& filename);
+void writeReport(const string& filename);
 
 #endif
